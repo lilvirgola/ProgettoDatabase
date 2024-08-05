9c02e465aa62b8e53a5d98c4be3899d35289c62f
lilvirgola <alessandro.debiasi@protonmail.com>
2024-08-05 18:50:49 +0200

    sistemata query 1

--- codice/query/Query_1.sql
+++ codice/query/Query_1.sql
@@ -1,6 +1,6 @@
--- query 1 : trovare tutti gli aeroporti in cui è passato mario rossi con voli diretti a tokio
+-- query 1 : trovare tutti gli aeroporti in cui è passato mario rossi con voli diretti a Huanggang
 
-SELECT DISTINCT A.nome
+SELECT A.nome
 FROM Passeggero P
 JOIN Prenotazione pren ON P.id_passeggero=pren.passeggero 
 JOIN Comprende C ON C.id_prenotazione=pren.id_prenotazione
@@ -9,5 +9,5 @@ JOIN Tratta TR ON TR.id_tratta=it.id_tratta
 JOIN Aeroporto A ON (A.codice_aeroporto = TR.aeroporto_arrivo OR A.codice_aeroporto=TR.aeroporto_partenza)
 JOIN Volo V ON V.id_volo=pren.riguarda_volo
 JOIN Aeroporto Avolo ON V.aeroporto_arrivo = Avolo.codice_aeroporto
-WHERE P.nome = 'Mario' AND P.cognome = 'Rossi'
-    AND Avolo.citta='Tokio'
+WHERE P.nome = 'clorindo' AND P.cognome = 'scipione'
+    AND Avolo.citta='Huanggang'
