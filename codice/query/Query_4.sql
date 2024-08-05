294f67e5c678694e57d034b73958b253d964b04e
lilvirgola <alessandro.debiasi@protonmail.com>
2024-08-05 17:40:41 +0200

    modificata query 4

--- codice/query/Query_4.sql
+++ codice/query/Query_4.sql
@@ -1,18 +1,18 @@
--- query 4 : Trovare una tra le compagnie aere che effettua più atterraggi a Sydney
-CREATE OR REPLACE VIEW atterraggi_sydney AS
+-- query 4 : Trovare una tra le compagnie aere che effettua più atterraggi a Seoul
+CREATE OR REPLACE VIEW atterraggi_seoul AS
 SELECT COUNT(*) AS num_atterraggi, ca.id_compagnia
 FROM Compagnia_Aerea AS ca
 JOIN Volo AS v ON ca.id_compagnia=v.id_compagnia
 JOIN Compone AS c ON v.id_volo=c.id_volo
 JOIN Tratta AS tr ON tr.id_tratta=c.id_tratta
 JOIN Aeroporto AS ar ON ar.codice_aeroporto=tr.aeroporto_arrivo
-WHERE ar.citta='Sydney'
+WHERE ar.citta='Seoul'
 GROUP BY ca.id_compagnia;
 
 SELECT nome, num_atterraggi
 FROM Compagnia_Aerea AS ca
-JOIN atterraggi_sydney AS ats ON ca.id_compagnia=ats.id_compagnia
+JOIN atterraggi_seoul AS ats ON ca.id_compagnia=ats.id_compagnia
 WHERE ats.num_atterraggi = (
     SELECT max(num_atterraggi)
-    FROM atterraggi_sydney
+    FROM atterraggi_seoul
 );
